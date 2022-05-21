a728b6af0ca282bd63d1e392d3ad46842942fc70
Sanlee Sa <sanlee.r.sa@gmail.com>
2022-05-21 14:07:55 +0530

    Created and styled header

--- src/common/Header/Header.js
+++ src/common/Header/Header.js
@@ -1,11 +1,18 @@
-import React from 'react'
+import React, { Fragment } from 'react'
+import './Header.css'
+import logo from '../../assets/logo.svg';
+import { Button } from '@material-ui/core';
 
 function Header() {
+  
   return (
-    <div className='header-div'>
-      
-    </div>
+    <Fragment>
+      <div className='header-div'>
+        <img src={logo} className="logo" alt="logo" />
+        <Button variant="contained" className='header-btn'>Login</Button>
+      </div>
+    </Fragment>
   )
 }
 
-export default Header();
+export default Header
