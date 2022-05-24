903e9538959eef13704bdee417f94561b81fb3a8
Sanlee Sa <sanlee.r.sa@gmail.com>
2022-05-24 12:13:08 +0530

    cleaned up home component

--- src/screens/home/Home.js
+++ src/screens/home/Home.js
@@ -1,10 +1,57 @@
-import React from 'react'
-import Header from '../../common/header/Header'
+import React, { Fragment, useEffect, useState } from 'react';
+import Header from '../../common/header/Header';
+import './Home.css';
+import { makeStyles } from "@material-ui/styles";
+import UpcomingMovies from './home-component/UpcomingMovies';
+
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    margin: 0,
+    flexWrap: "nowrap",
+  }
+}));
 
 function Home() {
+
+  const classes = useStyles();
+
+  const [movieData, setMovieData] = useState([]);
+
+  let tileData = [];
+
+  const getUpComingMovie = async () => {
+    try {
+      const rawResponse = await fetch('http://localhost:8085/api/v1/movies?page=1&limit=10');
+      const jsonData = await rawResponse.json();
+      return jsonData;
+    } catch (error) {
+      throw new Error(error);
+    }
+  }
+
+  useEffect(() => {
+    getUpComingMovie().then((movies) => {
+      setMovieData(movies.movies)
+    });
+  }, [])
+
+  movieData.forEach(element => {
+    tileData.push(element);
+  });
+  
+
   return (
-    <Header />
+    <Fragment>
+      <Header />
+      <div className='home-heading'>
+        <span className='heading-text'>
+          Upcoming Movies
+        </span>
+      </div>
+      <UpcomingMovies classes={classes} tileData={tileData} />
+    </Fragment>
   )
 }
 
-export default Home
+export default Home;
