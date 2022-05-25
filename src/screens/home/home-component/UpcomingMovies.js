14d3628ad406f08ff8329a9160383b12d0f5a6e3
Sanlee Sa <sanlee.r.sa@gmail.com>
2022-05-25 21:38:43 +0530

    fixed bugs

--- src/screens/home/home-component/UpcomingMovies.js
+++ src/screens/home/home-component/UpcomingMovies.js
@@ -1,18 +1,27 @@
 import React, { Fragment } from 'react';
-import ImageList from "@material-ui/core/ImageList";
-import ImageListItem from "@material-ui/core/ImageListItem";
-import ImageListItemBar from "@material-ui/core/ImageListItemBar";
+import { makeStyles } from '@material-ui/styles'
+import ImageList from '@material-ui/core/ImageList';
+import ImageListItem from '@material-ui/core/ImageListItem';
+import ImageListItemBar from '@material-ui/core/ImageListItemBar';
 
-function UpcomingMovies({ classes, tileData }) {
+const useStyles = makeStyles(theme => ({
+  root: {
+    margin: 0,
+    flexWrap: "nowrap",
+  },
+}));
+
+function UpcomingMovies({ tileData }) {
+  const classes = useStyles();
   return (
     <Fragment>
       <ImageList
         className={classes.root}
         style={{ margin: 0 }}
         cols={6}
-        cellHeight={250}>
+        rowHeight={250}>
         {(tileData || []).map(movie => (
-          <ImageListItem key={movie.id} className={classes.gridList}>
+          <ImageListItem key={movie.id}>
             <img src={movie.poster_url} alt={movie.title} />
             <ImageListItemBar title={movie.title} />
           </ImageListItem>
